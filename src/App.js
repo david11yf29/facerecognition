69c515d6f5a89b88b22b06a56f39bcacb8500cdc
david11yf29 <david11yf29@gmail.com>
2020-07-05 17:48:45 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import Navigation from './components/Navigation/Navigation';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
