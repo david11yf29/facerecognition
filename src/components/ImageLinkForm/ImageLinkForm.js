3d2601717e43a25e2153b58873552a3a8caf839d
david11yf29 <david11yf29@gmail.com>
2020-07-05 18:55:28 +0800

    updated

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ImageLinkForm.css';
  
 const ImageLinkForm = () => {
     return (
@@ -7,8 +8,10 @@ const ImageLinkForm = () => {
                 {'This magic Brain will detect faces in your pictures. Give it a try'}
             </p>
             <div className="center">
-                <input className="f4 pa2 w-70 center" type="text" />
-                <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                <div className="form center pa4 br3 shadow-5">
+                    <input className="f4 pa2 w-70 center" type="text" />
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                </div>
             </div>
         </div>
     )
