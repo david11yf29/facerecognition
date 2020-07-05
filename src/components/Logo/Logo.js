fd91ca0d11eadd8981da2f6f736ef60422435691
david11yf29 <david11yf29@gmail.com>
2020-07-05 18:20:58 +0800

    updated

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Tilt from 'react-tilt'
+ 
 const Logo = () => {
     return (
-        <div>
-            
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> 👽 </div>
+            </Tilt>
         </div>
     )
 }
