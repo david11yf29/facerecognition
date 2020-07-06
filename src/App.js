d68939e74283e480673982be981bc8aa8efa3120
david11yf29 <david11yf29@gmail.com>
2020-07-06 16:36:36 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ class App extends React.Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -81,6 +82,15 @@ class App extends React.Component {
   }
 
   onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({
+        isSignedIn: false
+      })
+    } else if (route === "home") {
+      this.setState({
+        isSignedIn: true
+      })
+    }
     this.setState({
       route: route
     })
@@ -92,7 +102,7 @@ class App extends React.Component {
         <Particles 
           className="particles"
           params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         {
           this.state.route === "home" ? 
           <div>
