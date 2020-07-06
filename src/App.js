3cb0adac5085e4523875e81d859253a3b2eb013c
david11yf29 <david11yf29@gmail.com>
2020-07-06 14:39:07 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -47,8 +47,15 @@ class App extends React.Component {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom.row * height)
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({
+      box: box 
+    })
   }
 
   onInputChange = (event) => {
@@ -66,7 +73,7 @@ class App extends React.Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(response => this.calculateFaceLocation(response))
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
       .catch(err => console.log(err));
   }
 
